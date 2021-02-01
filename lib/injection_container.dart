9986c9bf2173ea3e816e88fb61f3e664e52b5ffc
HC Febrian <febriansyah.online@gmail.com>
2021-02-01 16:50:28 +0700

    inject all the dependency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,16 +5,24 @@ import 'package:flutter_text_recognition/data/data_source/local/camera_platform/
 import 'package:flutter_text_recognition/data/data_source/local/camera_platform/camera_platform_impl.dart';
 import 'package:flutter_text_recognition/data/data_source/local/ml_kit_local/ml_kit_local_abs.dart';
 import 'package:flutter_text_recognition/data/data_source/local/ml_kit_local/ml_kit_platform_impl.dart';
+import 'package:flutter_text_recognition/data/data_source/remote/pizza_history_remote/pizza_order_data_source_abs.dart';
+import 'package:flutter_text_recognition/data/data_source/remote/pizza_history_remote/pizza_order_data_source_impl.dart';
 import 'package:flutter_text_recognition/data/data_source/remote/purchase_order_remote/purchase_order_remote_abs.dart';
 import 'package:flutter_text_recognition/data/data_source/remote/purchase_order_remote/purchase_order_remote_impl.dart';
 import 'package:flutter_text_recognition/data/repository/canera_repo_impl.dart';
 import 'package:flutter_text_recognition/data/repository/mlkit_repo_impl.dart';
+import 'package:flutter_text_recognition/data/repository/pizza_history_repo.dart';
 import 'package:flutter_text_recognition/data/repository/purchase_repo_impl.dart';
+import 'package:flutter_text_recognition/data/repository/similarity_repo_impl.dart';
 import 'package:flutter_text_recognition/domain/contract_repository/camera_repo_abs.dart';
 import 'package:flutter_text_recognition/domain/contract_repository/mlkit_repo_abs.dart';
+import 'package:flutter_text_recognition/domain/contract_repository/pizza_repo_abs.dart';
 import 'package:flutter_text_recognition/domain/contract_repository/purchase_repo_abs.dart';
+import 'package:flutter_text_recognition/domain/contract_repository/similarity_repo_abs.dart';
+import 'package:flutter_text_recognition/domain/usecase/order_history_usecase.dart';
 import 'package:flutter_text_recognition/domain/usecase/purchase_scan_usecase.dart';
-import 'package:flutter_text_recognition/presentation/bloc/bloc_similarity_image/process_image_bloc.dart';
+import 'package:flutter_text_recognition/presentation/bloc/pizza_history/pizza_history_bloc.dart';
+import 'package:flutter_text_recognition/presentation/bloc/scaner/scanner_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -23,26 +31,34 @@ final sl = GetIt.instance;
 void init() {
 // Feature
   //bloc
-  sl.registerFactory(() => SimilarityImageBloc(purchaseUsecase: sl()));
+  sl.registerFactory(() => PizzaHistoryBloc(sl()));
+  sl.registerFactory(() => ScannerBloc(purchaseScanUsecase: sl()));
 
   //UseCase
-  sl.registerLazySingleton(() => PurchaseScanUsecase(
+  sl.registerLazySingleton<OrderHistoryUsecase>(
+      () => OrderHistoryUsecase(pizzaRepoAbs: sl()));
+  sl.registerLazySingleton<PurchaseScanUsecase>(() => PurchaseScanUsecase(
         jaccard: sl(),
         purchaseRepo: sl(),
         cameraRepoAbs: sl(),
-        levenshtein: sl(),
         mlkitRepoAbs: sl(),
+        similarityRepo: sl(),
+        pizzarepo: sl(),
       ));
 
   //repo
+  sl.registerLazySingleton<SimilarityRepoAbs>(() => SimilarityRepoImpl(jaccard: sl()));
+  sl.registerLazySingleton<PizzaRepoAbs>(
+      () => PizzaRepoImpl(pizzaDataSource: sl()));
   sl.registerLazySingleton<PurchaseRepoAbs>(
       () => PurchaseRepoImpl(purchaseRemoteAbs: sl()));
   sl.registerLazySingleton<CameraRepoAbs>(
       () => CameraRepoImpl(cameraPlatform: sl()));
-  sl.registerLazySingleton<MLRepoAbs>(
-      () => MLKitRepoImpl(mlKit: sl()));
+  sl.registerLazySingleton<MLRepoAbs>(() => MLKitRepoImpl(mlKit: sl()));
 
   //data
+  sl.registerLazySingleton<PizzaHistoryDataSourceAbs>(
+      () => PizzaHistoryDataSourceImpl(sl()));
   sl.registerLazySingleton<PurchaseRemoteAbs>(
       () => PurchaseRemoteDataImpl(firesBaseFirestore: sl()));
   sl.registerLazySingleton<CameraPlatformAbs>(
@@ -63,6 +79,4 @@ void init() {
   sl.registerLazySingleton(() => Levenshtein());
   sl.registerLazySingleton(() => Jaccard());
   sl.registerLazySingleton(() => FirebaseVision.instance.textRecognizer());
-
 }
-
