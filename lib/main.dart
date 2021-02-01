5c08ce869387113f16d2051eb415079ec1c2de21
HC Febrian <febriansyah.online@gmail.com>
2021-02-01 16:19:34 +0700

    add title and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_text_recognition/injection_container.dart';
-
-import 'presentation/bloc/bloc_similarity_image/process_image_bloc.dart';
+import 'package:flutter_text_recognition/core/colors.dart';
+import 'package:flutter_text_recognition/route_generator.dart';
 
 import 'injection_container.dart' as di;
-import 'presentation/pages/purchase_screen.dart';
-
 
 Future<void> main() async {
-  // Fetch the available cameras before initializing the app.
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   di.init();
+  print(hexToColor("#fbae0d").toString());
   runApp(MaterialApp(
-    home: MultiBlocProvider(
-      providers: [
-        BlocProvider<SimilarityImageBloc>(
-          create: (BuildContext context) => SimilarityImageBloc(purchaseUsecase: sl()),
-        ),
-      ],
-      child: PurchaseScreen(),
-    )
-    ,
-  ));
+      title: "Pizza App",
+      theme: ThemeData(
+        backgroundColor: appColorBackground,
+        primaryColor: appColorSecondaryDarkBlue,
+        accentColor: appColorPrimaryYellow
+      ),
+      initialRoute: "/",
+      onGenerateRoute: RouteGenerator.generateRoute));
 }
-
