25e39ad7f2e971bb70e98a64377927ad340f0284
HC Febrian <febriansyah.online@gmail.com>
2021-02-02 08:56:10 +0700

    add new entity

--- lib/domain/entity/similarity_result.dart
+++ lib/domain/entity/similarity_result.dart
@@ -1,20 +1,24 @@
-import 'dart:io';
 
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 class SimilarityResult extends Equatable {
   final double similarity;
-  final File imageFile;
-  final String textFromDb;
-  final String textFromMl;
+  final bool confirmed;
+  final String purchaseID;
+  final int cashback;
 
   SimilarityResult(
       {@required this.similarity,
-      @required this.imageFile,
-      @required this.textFromDb,
-      @required this.textFromMl});
+      @required this.confirmed,
+      @required this.purchaseID,
+      @required this.cashback});
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [
+        confirmed,
+        similarity,
+        purchaseID,
+        cashback,
+      ];
 }
