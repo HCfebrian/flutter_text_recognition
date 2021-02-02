4749205802d1f5de7fb450e5b922a979589f360c
HC Febrian <febriansyah.online@gmail.com>
2021-02-02 09:16:58 +0700

    this code get data from database using document Id.

--- lib/data/data_source/remote/purchase_order_remote/purchase_order_remote_impl.dart
+++ lib/data/data_source/remote/purchase_order_remote/purchase_order_remote_impl.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_text_recognition/data/data_source/remote/purchase_order_remote/purchase_order_remote_abs.dart';
-import 'package:flutter_text_recognition/data/model_converter/purchase_model.dart';
-import 'package:flutter_text_recognition/domain/entity/purchase_order.dart';
+import 'package:flutter_text_recognition/data/model_converter/pizza_converter.dart';
+import 'package:flutter_text_recognition/data/model_converter/purchase_entity_converter.dart';
+import 'package:flutter_text_recognition/domain/entity/pizza_entity.dart';
+import 'package:flutter_text_recognition/domain/entity/purchase_entity.dart';
 import 'package:meta/meta.dart';
 
 class PurchaseRemoteDataImpl extends PurchaseRemoteAbs {
@@ -11,10 +13,15 @@ class PurchaseRemoteDataImpl extends PurchaseRemoteAbs {
 
   @override
   Future<PurchaseEntity> getPurchaseDetail(String id) async {
+    print("get purchase detail running " + id);
     final purchaseHistory =
-        firesBaseFirestore.collection("purchase_history").doc(id);
-
+    firesBaseFirestore.collection("purchase_history").doc(id);
     final ds = await purchaseHistory.get();
-    return PurchaseModel.from(ds);
+    final purchaseCollection = await firesBaseFirestore.collection("purchase_history/$id/order_collection").get();
+    List<PizzaHistoryEntity> listPizza=[];
+    purchaseCollection.docs.forEach((element) {
+      listPizza.add(PizzaEntityConverter.from(element));
+    });
+    return PurchaseEntityConverter.from(ds, listPizza);
   }
 }
