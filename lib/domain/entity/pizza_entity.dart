35251a8b63f3b77d43d4e07c4c815b2adf5450fe
HCFebrian <febriansyah.online@gmail.com>
2021-02-05 14:01:08 +0700

    add business entity

--- lib/domain/entity/pizza_entity.dart
+++ lib/domain/entity/pizza_entity.dart
@@ -1,26 +1,27 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 class PizzaHistoryEntity extends Equatable {
-  final recipeId;
-  final purchasePicUrl;
-  final purchaseId;
-  final purchaseDate;
-  final purchaseQuantity;
-  final pizzaName;
-  final pizzaPrice;
-  final pizzaSize;
-  final pizzaCal;
+  final String recipeId;
+  final String purchasePicUrl;
+  final String purchaseId;
+  final String purchaseDate;
+  final String purchaseQuantity;
+  final String pizzaName;
+  final String pizzaPrice;
+  final String pizzaSize;
+  final String pizzaCal;
 
   PizzaHistoryEntity(
-      this.recipeId,
-      this.purchaseId,
-      this.purchaseDate,
-      this.purchaseQuantity,
-      this.pizzaName,
-      this.pizzaPrice,
-      this.pizzaSize,
-      this.pizzaCal,
-      this.purchasePicUrl);
+      {@required this.recipeId,
+      @required this.purchaseId,
+      @required this.purchaseDate,
+      @required this.purchaseQuantity,
+      @required this.pizzaName,
+      @required this.pizzaPrice,
+      @required this.pizzaSize,
+      @required this.pizzaCal,
+      @required this.purchasePicUrl});
 
   @override
   List<Object> get props => [
--- lib/domain/entity/purchase_entity.dart
+++ lib/domain/entity/purchase_entity.dart
@@ -1,7 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_text_recognition/domain/entity/pizza_entity.dart';
+import 'package:meta/meta.dart';
 
 class PurchaseEntity extends Equatable {
+  final String purchaseId;
   final List<PizzaHistoryEntity> listOrder;
   final String purchaseDate;
   final String purchaseTime;
@@ -11,23 +13,28 @@ class PurchaseEntity extends Equatable {
   final int subTotal;
   final int balanceDue;
 
-  PurchaseEntity(
-    this.listOrder,
-    this.purchaseDate,
-    this.workerId,
-    this.workerName,
-    this.purchaseTime,
-    this.subTotal,
-    this.balanceDue,
-    this.cashback,
-  );
+  PurchaseEntity({
+    @required this.purchaseId,
+    @required this.listOrder,
+    @required this.purchaseDate,
+    @required this.workerId,
+    @required this.workerName,
+    @required this.purchaseTime,
+    @required this.subTotal,
+    @required this.balanceDue,
+    @required this.cashback,
+  });
 
   @override
   List<Object> get props => [
+        purchaseId,
         listOrder,
         purchaseDate,
+        purchaseTime,
         workerId,
         workerName,
-        purchaseTime,
+        cashback,
+        subTotal,
+        balanceDue
       ];
 }
