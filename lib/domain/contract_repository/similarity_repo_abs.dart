05ce4f20f68dd582f91d417265c03a1d2de5cfd3
HCFebrian <febriansyah.online@gmail.com>
2021-02-05 14:03:23 +0700

    change all argument to one purchase entity to make it compact

--- lib/domain/contract_repository/similarity_repo_abs.dart
+++ lib/domain/contract_repository/similarity_repo_abs.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_text_recognition/domain/entity/pizza_entity.dart';
+import 'package:flutter_text_recognition/domain/entity/purchase_entity.dart';
 
 abstract class SimilarityRepoAbs {
   String getDbComparableText({
-    @required String purchaseID,
-    @required String workerName,
-    @required String workerId,
-    @required String purchaseDate,
-    @required String purchaseTime,
-    @required int subTotal,
-    @required int balanceDue,
-    @required List<PizzaHistoryEntity> listPizza,
+    @required PurchaseEntity purchaseEntity,
   });
 
   double getSimilarity(
